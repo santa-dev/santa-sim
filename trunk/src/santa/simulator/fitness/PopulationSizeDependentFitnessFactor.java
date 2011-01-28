cf3050057de4f904760c1e57546fcbc876a14266
gertjan.beheydt@gmail.com <gertjan.beheydt@gmail.com@b2144b69-1825-0410-ba2e-6bd7d9121048>
2011-01-28 10:28:53 +0000

    fitness factor dependent on the population size

---
+++ trunk/src/santa/simulator/fitness/PopulationSizeDependentFitnessFactor.java
@@ -0,0 +1,49 @@
+package santa.simulator.fitness;
+
+import java.util.Set;
+
+import santa.simulator.genomes.Feature;
+import santa.simulator.genomes.StateChange;
+import santa.simulator.population.Population;
+
+public class PopulationSizeDependentFitnessFactor extends AbstractFitnessFactor {
+
+	private int max;
+	private double declineRate;
+	
+	private int populationSize;
+	private double logFitness;
+	
+	
+	public PopulationSizeDependentFitnessFactor(int max, double declineRate, Feature feature, Set<Integer> sites) {
+		super(feature, sites);
+		this.max = max;
+		this.declineRate = declineRate;
+	}
+	
+	public boolean updateGeneration(int generation, Population population) {
+		//we should only be updating if population size changed
+		if(population.getPopulationSize() != populationSize) {
+			populationSize = population.getPopulationSize();
+			// get the population mean fitness without the contribution of this factor in the previous generation
+			double meanFitness = population.getMeanFitness() - logFitness;
+			// calculate the new factor
+			GeneralisedLogisticFunction glf = new GeneralisedLogisticFunction(1-meanFitness, 0, -declineRate, max);
+			logFitness = glf.getFunctionValue(populationSize);
+			//System.err.println("DEBUG: "+populationSize+" "+meanFitness+" "+logFitness);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public double computeLogFitness(byte[] states) {		
+		return logFitness;
+	}
+
+	@Override
+	public double getLogFitnessChange(StateChange change) {
+		return logFitness;
+	}
+	
+}
