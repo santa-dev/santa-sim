ebb0e4df5a0fb7e684cf5a58ea91a0088de7ca25
jariani.abb@gmail.com <jariani.abb@gmail.com@b2144b69-1825-0410-ba2e-6bd7d9121048>
2013-11-19 10:45:21 +0000

    Implementation of dynamics of logistic growth of population

--- trunk/src/santa/simulator/selectors/DynamicSelector.java
+++ trunk/src/santa/simulator/selectors/DynamicSelector.java
@@ -5,13 +5,26 @@ import java.util.List;
 
 import santa.simulator.Random;
 import santa.simulator.Virus;
-
+/**
+ * 
+ * @author abbas
+ *	implementation of population dynamics:
+ *	E(number of progeny per parent) = (1 + r (1-PopulationSize/CarryingSize)) * fitness of the parrent
+ *	where r is the growth rate per generation
+ * 
+ */
 public class DynamicSelector implements Selector {
 
+	//Look through literature for estimates of growth rate per generation 
+	double growthRate = 5;
+	//value of the carrying population would depend on the type of the question under investigation
+	double carryingPopulation = 1000;
+	double expectedProgenyCount;
 	public void selectParents(List<Virus> currentGeneration, List<Integer> selectedParents, int nbOfParents) {
 		for(int i = 0; i < currentGeneration.size(); ++i) {
 			double fitness = currentGeneration.get(i).getFitness();
-			long nbChildren = fitness == 0 ? 0 : Random.nextPoisson(fitness);
+			expectedProgenyCount =  fitness * (1 + growthRate*(1-selectedParents.size()/carryingPopulation));
+			long nbChildren = fitness == 0 ? 0 : Random.nextPoisson(expectedProgenyCount);
 			for(long n = 0; n < nbChildren * nbOfParents; ++n) {
 				selectedParents.add(i);				
 			}			
