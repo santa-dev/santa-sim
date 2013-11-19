b4b09a1d710839d019ee932af44939f897602d56
jariani.abb@gmail.com <jariani.abb@gmail.com@b2144b69-1825-0410-ba2e-6bd7d9121048>
2013-11-19 11:30:43 +0000

    Handling of the case where expected number of progeny for a parent is nonpositive

--- trunk/src/santa/simulator/selectors/DynamicSelector.java
+++ trunk/src/santa/simulator/selectors/DynamicSelector.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import santa.simulator.Random;
 import santa.simulator.Virus;
+
 /**
  * 
  * @author abbas
@@ -16,14 +17,14 @@ import santa.simulator.Virus;
 public class DynamicSelector implements Selector {
 
 	//Look through literature for estimates of growth rate per generation 
-	double growthRate = 5;
+	double growthRate = 1000;
 	//value of the carrying population would depend on the type of the question under investigation
 	double carryingPopulation = 1000;
 	double expectedProgenyCount;
 	public void selectParents(List<Virus> currentGeneration, List<Integer> selectedParents, int nbOfParents) {
 		for(int i = 0; i < currentGeneration.size(); ++i) {
 			double fitness = currentGeneration.get(i).getFitness();
-			expectedProgenyCount =  fitness * (1 + growthRate*(1-selectedParents.size()/carryingPopulation));
+			expectedProgenyCount =  Math.max(fitness * (1 + growthRate*(1-selectedParents.size()/carryingPopulation)),Double.MIN_VALUE);
 			long nbChildren = fitness == 0 ? 0 : Random.nextPoisson(expectedProgenyCount);
 			for(long n = 0; n < nbChildren * nbOfParents; ++n) {
 				selectedParents.add(i);				
